8b0f18a0c27134a9e1816af4ca8132ccb6564de0
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2022-09-30 00:54:48 +0200

    Add files via upload

---
+++ FileProg.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char info[][30] = {"Misha", "Sidelnikov", "POKS-31", "18 y.o"};
+	char buff[1000];
+
+	FILE *fp;
+
+	fp = fopen("WhoAmI.txt", "w");
+	for(int i = 0; i <= sizeof(info)/sizeof(*info); i++)
+		fprintf(fp, "%s\n", info[i]);
+
+	fclose(fp);
+	
+	fp = fopen("WhoAmI.txt", "r");
+
+	while(!feof(fp))
+	{
+		if (fgets(buff, 1000,  fp) != NULL)
+			printf("%s", buff);
+	}
+	fclose(fp);
+	return 0;
+
+}
